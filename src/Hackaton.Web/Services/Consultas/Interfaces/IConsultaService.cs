6f6f0c16ed2a4fca46b80613edbdc919b45ad5ed
Giuliano Pongeluppe <giuliano_pongeluppe@hotmail.com>
2025-05-03 14:12:25 -0300

    Inclui cancelamento de consulta e finaliza lógica para mostrar horários para consulta

--- src/Hackaton.Web/Services/Consultas/Interfaces/IConsultaService.cs
+++ src/Hackaton.Web/Services/Consultas/Interfaces/IConsultaService.cs
@@ -5,5 +5,6 @@ namespace Hackaton.Web.Services.Consultas.Interfaces
     public interface IConsultaService
     {
         Task<IList<ConsultaPacienteModel>> ObterConsultasFuturasDoPaciente(int pacienteId);
+        Task CancelarConsulta(int consultaId, string motivo);
     }
 }
